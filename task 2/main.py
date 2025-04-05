eaca9aa04962f0961ffa9c14876c4f8377d86288
Jakub Stec <stecu03@gmail.com>
2025-04-05 15:38:36 +0200

    Start zad 2

---
+++ task 2/main.py
@@ -0,0 +1,6 @@
+from  src.loader import loadExperiment 
+
+exp1=loadExperiment('data/experiment_1.csv')
+exp2=loadExperiment('data/experiment_2.csv')
+exp3=loadExperiment('data/experiment_3.csv')
+print(exp1.time)
